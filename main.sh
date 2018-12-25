01a67aa09905f60dcaa62cdab92bd163d0528852
Thomas Moeller <thomas.moeller.r@gmail.com>
2018-12-25 20:30:32 +0000

    test

---
+++ main.sh
@@ -0,0 +1,8 @@
+sudo python3 "/home/pi/MQTT_Thermostat/main.py" \
+--mqtt_server_ip "192.168.2.52" \
+--mqtt_server_port "1883" \
+--mqtt_topic_set_temperature "thermostat/set_temperature" \
+--mqtt_topic_ack_temperature "thermostat/ack_temperature" \
+--thermostat_pin_a "16" \
+--thermostat_pin_b "18" \
+--thermostat_delay "0.03" \
---
+++ tmux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+tmux new-session -d -s my_session "bash /home/pi/MQTT_Thermostat/main.sh"
+exit 0
